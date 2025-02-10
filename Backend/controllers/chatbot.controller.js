7c4815771a62f7456e7ac6096170617c6ad80061
ritik-gupta-2022 <aggupta0936@gmail.com>
2025-02-11 02:06:51 +0530

    feat: add filter question functionality and delete helpdesk endpoint

--- Backend/controllers/chatbot.controller.js
+++ Backend/controllers/chatbot.controller.js
@@ -1,5 +1,6 @@
 import Business from "../models/business.model.js";
 import ChatBot from "../models/chatbot.model.js";
+import FilterQuestions from "../models/filterQuestion.model.js";
 import HelpDesk from "../models/helpDesk.model.js";
 
 export const createChatbot = async(userId , businessId) =>{
@@ -20,7 +21,7 @@ export const addhelpdesk = async (req, res, next) => {
       const businessId = req.params.businessId;
       const {question , answer} = req.body;
       
-      if(question === "" || answer === ""){
+      if(question.trim() === "" || answer.trim() === ""){
         return next(errorHandler(400, "All fields are required"));
       }
 
@@ -54,6 +55,69 @@ export const getHelpDesk = async (req, res, next) => {
          return next(errorHandler(404, "Business not found"));
       }
       res.status(200).json(business.helpdesk);
+   }
+   catch(err){
+      next(err);
+   }
+}
+
+export const deleteHelpDesk = async (req, res, next) => {   
+   const helpDeskId = req.params.helpDeskId;
+   const businessId = req.params.businessId;
+   try {
+      const business = await Business.findById(businessId);
+      if (!business) {
+         return next(errorHandler(404, "Business not found"));
+      }
+      const helpdesk = await HelpDesk.findById(helpDeskId);
+      if (!helpdesk) {
+         return next(errorHandler(404, "HelpDesk not found"));
+      }
+      await HelpDesk.findByIdAndDelete(helpDeskId);
+      business.helpdesk.pull(helpDeskId);
+      await business.save();
+      res.status(200).json(helpdesk);
+   }
+   catch(err){
+      next(err);
+   }
+}
+
+export const addfilterquestion = async(req,res,next)=>{
+   const businessId = req.params.businessId;
+   const {question} = req.body;
+
+   if(question.trim() === ""){
+      return next(errorHandler(400, "All fields are required"));
+   }
+
+   try{
+      const business = await Business.findById(businessId);
+      if (!business) {
+         return next(errorHandler(404, "Business not found"));
+      }
+      const filterQuestion = new FilterQuestions({
+         question,
+         businessId
+      });
+      await filterQuestion.save();
+      business.filterQuestions.push(filterQuestion._id);
+      await business.save();
+      res.status(201).json(filterQuestion);
+   }
+   catch(err){
+      next(err);
+   }
+}
+
+export const getFilterQuestion = async (req, res, next) => {
+   const businessId = req.params.businessId;
+   try {
+      const business = await Business.findById(businessId).populate("filterQuestions");
+      if (!business) {
+         return next(errorHandler(404, "Business not found"));
+      }
+      res.status(200).json(business.filterQuestions);
    }
    catch(err){
       next(err);
--- Backend/routes/chatbot.route.js
+++ Backend/routes/chatbot.route.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { addhelpdesk, getHelpDesk } from "../controllers/chatbot.controller.js";
+import { addfilterquestion, addhelpdesk, deleteHelpDesk, getFilterQuestion, getHelpDesk } from "../controllers/chatbot.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/addhelpdesk/:businessId", verifyToken,addhelpdesk);
 router.get("/getHelpDesk/:businessId", getHelpDesk);	
+router.delete("/deleteHelpDesk/:businessId/:helpDeskId", verifyToken, deleteHelpDesk);
+router.post("/addfilterquestion/:businessId", verifyToken,addfilterquestion);
+router.get("/addfilterquestion/:businessId",getFilterQuestion);
 
 
 export default router;
